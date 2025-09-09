6128bdf087c7a940293ef7035cdad0c151286d7f
Jesnie <jesniemagaling05@gmail.com>
2025-09-09 17:29:48 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <div className="flex h-screen items-center justify-center bg-gray-900">
+        <h1 className="text-4xl font-bold text-blue-400">
+          Tailwind is working 🎉
+        </h1>
+        <h2 className="text-primary-light dark:text-primary-dark text-3xl font-bold">
+          Hello, I’m Jesnie 👋
+        </h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
