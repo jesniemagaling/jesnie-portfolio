b3005cd14037a16e3f4fca47ebef8b7a039c7330
Jesnie <jesniemagaling05@gmail.com>
2025-09-12 22:57:56 +0800

    feat: make section header as components

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,15 +1,5 @@
-function Projects() {
-  return (
-    <section className="">
-      <h1 className="text-[28px] font-bold md:text-[32px] lg:text-4xl">
-        Notable Project
-      </h1>
-      <p className="text-sm opacity-80 dark:opacity-70 md:text-xl">
-        Here’s a quick look at some of the projects I’ve worked on. They show
-        the kind of work I enjoy and the skills I do best.
-      </p>
-    </section>
-  );
-}
+import SectionHeader from "./SectionHeader";
 
-export default Projects;
+export default function Projects() {
+  return <></>;
+}
---
+++ src/components/SectionHeader.tsx
@@ -0,0 +1,22 @@
+interface SectionHeaderProps {
+  title: String;
+  description: String;
+}
+
+export default function SectionHeader({
+  title,
+  description,
+}: SectionHeaderProps) {
+  return (
+    <>
+      <div className="space-y-1 text-center md:text-left">
+        <h1 className="text-[28px] font-bold md:text-[32px] lg:text-4xl">
+          {title}
+        </h1>
+        <p className="text-sm opacity-80 dark:opacity-70 md:text-xl">
+          {description}
+        </p>
+      </div>
+    </>
+  );
+}
