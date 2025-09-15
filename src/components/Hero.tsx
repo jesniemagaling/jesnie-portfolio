5e64f570ae1f8c1795385bdcbbdc534859394ae6
Jesnie <jesniemagaling05@gmail.com>
2025-09-15 23:36:26 +0800

    feat: add animation to hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,12 +2,24 @@ import { PrimaryButton, SecondaryButton } from "./CustomButtons";
 import gsap from "gsap";
 import { useEffect, useRef } from "react";
 
-function Hero() {
+export default function Hero() {
+  const titleRef = useRef<HTMLHeadingElement>(null);
+
+  useEffect(() => {
+    gsap.fromTo(
+      titleRef.current,
+      { y: 80, opacity: 0 },
+      { y: 0, opacity: 1, duration: 1.2, ease: "power3.out" },
+    );
+  }, []);
+
   return (
     <section className="flex h-full min-h-screen items-center justify-center">
       <div className="mx-auto max-w-[1000px] space-y-6">
         <div className="font-bold">
-          <p className="text-[44px] md:text-[60px]">Hi, I’m Jesnie,</p>
+          <p ref={titleRef} className="text-[44px] md:text-[60px]">
+            Hi, I’m Jesnie,
+          </p>
           <p className="text-[48px] leading-none md:text-[64px] md:leading-10">
             Full-Stack <span className="text-accent-dark">Developer.</span>
           </p>
@@ -36,5 +48,3 @@ function Hero() {
     </section>
   );
 }
-
-export default Hero;
